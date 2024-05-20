cda4f6c8039e56c9616a16331089aaf8f619f651
Rani <rani7042003@gmail.com>
2024-05-20 23:02:04 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Page2 from './components/Page2'
 import Page3 from './components/Page3'
 import Certificate from './components/Certificate'
+import Last from './components/Last'
 const App = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const App = () => {
       <Page2/>
       <Page3/>
       <Certificate/>
+      <Last/>
     </>
   )
 }
---
+++ src/components/Last.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Last = () => {
+  return (
+    <div>
+        <img src="./graph.png" alt="" />
+    </div>
+  )
+}
+
+export default Last
